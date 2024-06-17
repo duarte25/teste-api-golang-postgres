84ceaf058522b490836713dfa894699a7702e1af
gustavo <duarte.guga2025@gmail.com>
2024-06-17 16:11:23 -0400

    Primeiro commit da api teste

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/api_postgresql/configs"
+	"github.com/api_postgresql/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	err := configs.Load()
+
+	if err != nil {
+		panic(err)
+	}
+
+	r := chi.NewRouter()
+	r.Post("/", handlers.Create)
+	r.Put("/{id}", handlers.Update)
+	r.Delete("/{id}", handlers.Delete)
+	r.Get("/", handlers.List)
+	r.Get("/{id}", handlers.Get)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
+
+}
